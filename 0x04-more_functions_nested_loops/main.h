fa5899170f6e35a6589430572ca1b8b2c11fccb3
lordanas001 <anashalim100@gmail.com>
2023-08-18 09:15:24 -0700

    main.h file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,16 @@
 #define _MAIN_H_
 
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char);
+void print_most_numbers(void);
+void print_linr(int n);
+void print_diagonal(int n);
+void print_dquare(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
 _
