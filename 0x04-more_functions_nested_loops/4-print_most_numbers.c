4c635a8da514abe82e4e114be546af81f2cd77da
lordanas001 <anashalim100@gmail.com>
2023-08-18 15:58:38 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_most_numbers (void)
+{
+int number;
+for (number = 0; number < 10; number++)
+{
+if ((number == 2) || (number == 4))
+{
+continue;
+}
+_putchar (number);
+}
+_putchar (10);
+}
