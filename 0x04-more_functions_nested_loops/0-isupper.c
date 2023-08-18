539f15edfc9e40d2e1eb4c23a40585a4c7b79b51
lordanas001 <anashalim100@gmail.com>
2023-08-18 14:49:11 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,12 @@
 #include "main.h"
-<<<<<<< HEAD
 #include <stdio.h>
 
-=======
-/*
+/**
  * _isupper -  checkes upper case
  * @c: print int
  * Return: give 1 or 0
 */
->>>>>>> f2026c34d2cb188f8c6f3fb1879350ad92f0ae0e
+
 int _isupper(int c)
 {
 if(c >= 'A' && c <= 'Z')
