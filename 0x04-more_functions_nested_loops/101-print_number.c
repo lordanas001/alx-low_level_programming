95041e4a3753b1803a051136b9f19a7d9c5f6032
lordanas001 <anashalim100@gmail.com>
2023-08-18 17:24:22 -0700

    task12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_number (int n)
+{
+unsigned int m;
+if (n < 0)
+{
+_putchar('-');
+m = -n;
+}
+else
+{
+m = n;
+}
+if (m/ 10 = 0) 
+print_number(m/10);
+_putchar((m % 10) + '0');
+}
